d4bf5a91da61c65020fe5e93d72601447042ff2b
micaellagouveia <micaella2212@gmail.com>
2022-02-05 12:20:20 -0300

    Add shortestPath bfs function

--- front/src/components/Graph/Graph.js
+++ front/src/components/Graph/Graph.js
@@ -2,9 +2,10 @@ class Graph {
     constructor(node) {
         this.node = node;
         this.listAdj = new Map();
+        this.links = 0
     }
 
-    getGraph(){
+    getGraph() {
         return this.Graph;
     }
     addNode(v) {
@@ -14,26 +15,99 @@ class Graph {
     addLink(vertex, node) {
         this.listAdj.get(vertex).push(node);
         this.listAdj.get(node).push(vertex);
+        this.links++;
     }
 
-    BFS(firstNode) {
+    BFS(firstNode, lastNode) {
         const nodesQueue = [];
         const visited = new Map();
 
         visited[firstNode] = true;
         nodesQueue.push(firstNode);
 
-        while (!nodesQueue.isEmpty()) {
+        while (nodesQueue.length) {
             const vertex = nodesQueue.shift();
-            const nodesVertex = this.listAdj.get(vertex);
+            console.log("VERTEX -> ", vertex);
 
-            for (let node in nodesVertex) {
-                if (!visited[nodesVertex[node]]) {
-                    visited[nodesVertex[node]] = true;
-                    nodesQueue.push(nodesVertex[node]);
+            if (vertex == lastNode) return true;
+
+
+            const vertexList = this.listAdj.get(vertex);
+
+            for (let node in vertexList) {
+                if (!visited[vertexList[node]]) {
+                    visited[vertexList[node]] = true;
+                    nodesQueue.push(vertexList[node]);
                 }
             }
         }
+
+        return false;
+    }
+
+    shortestPathBFS(firstNode, lastNode) {
+        const nodesQueue = [firstNode];
+
+        const visited = new Map();
+        visited[firstNode] = true;
+
+        const links = new Map();
+        links[firstNode] = 0;
+
+        const previous = new Map();
+        previous[firstNode] = null;
+
+
+        const shortestPath = () => {
+            const pathStack = [lastNode]
+
+            let node = previous[lastNode];
+
+            while (node != firstNode) {
+                pathStack.push(node);
+                node = previous[node]
+            }
+            pathStack.push(firstNode);
+
+            // return shortest path
+            return pathStack.reverse().join(' > ')
+
+        }
+
+
+        while (nodesQueue.length) {
+            const vertex = Number(nodesQueue.shift());
+
+            if (vertex != lastNode) {
+                const vertexList = this.listAdj.get(vertex);
+
+                for (let node in vertexList) {
+                    const vNode = vertexList[node];
+
+                    if (!visited[vNode]) {
+                        // set as visited
+                        visited[vNode] = true;
+
+                        // add on queue
+                        nodesQueue.push(vNode);
+
+                        // add link
+                        links[vNode] = links[vertex] + 1
+
+                        // set previous path
+                        previous[vNode] = vertex
+                    }
+                }
+            }
+            else {
+                return {
+                    distance: links[lastNode],
+                    path: shortestPath()
+                };
+            }
+        }
+
+        return false;
     }
 }
 
